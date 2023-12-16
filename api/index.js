d78adfba78acd7985d5ede6373476d01cef96911
Prasanna6060 <ranjitkhanal61@gmail.com>
2023-12-16 11:53:33 +0545

    create signup api route and store it in database

--- api/index.js
+++ api/index.js
@@ -1,22 +1,33 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotEnv from 'dotenv';
-import userRouter from './routes/user.routes.js'
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.route.js';
 
 dotEnv.config();
 
-mongoose.connect(process.env.MONGOOSE).then(() =>{
-    console.log("Connected to MongoDb");
-}).catch(err => {
-    console.log(err);
-})
+mongoose.connect(process.env.MONGOOSE, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch(err => {
+        console.error("Error connecting to MongoDB:", err);
+    });
 
+mongoose.connection.on('error', err => {
+    console.error("MongoDB connection error:", err);
+});
 
 const app = express();
+
+app.use(express.json());
+
+
 const port = 3000;
 
-app.listen(port, () =>{
-    console.log("Server Listening on port" + port);
-})
+app.listen(port, () => {
+    console.log("Server listening on port " + port);
+});
 
-app.use('/api/user', userRouter );
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
