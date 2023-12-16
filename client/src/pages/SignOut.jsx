01f97c46865e3208639d92c4bd933f6615fdc018
Prasanna6060 <ranjitkhanal61@gmail.com>
2023-12-16 13:04:53 +0545

    Create sign up UI

--- client/src/pages/SignOut.jsx
+++ client/src/pages/SignOut.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const SignUp = () => {
   return (
-    <div>
-      Signup
+    <div className='p-3 mx-auto max-w-lg'>
+      <h1 className='font-semibold text-center text-3xl my-7'>Sign Up</h1>
+      <form className='flex flex-col gap-4' >
+       <input type="text" placeholder='username' id='username' 
+       className='rounded-lg border p-3' />
+       <input type="text" placeholder='email' id='email' 
+       className='rounded-lg border p-3' />
+       <input type="text" placeholder='password' id='password' 
+       className='rounded-lg border p-3' />
+       <button className='text-white bg-slate-700 p-3 rounded-lg hover:opacity-90 disabled:opacity-80 
+       uppercase'
+       >Sign Up</button>
+      </form>
+      <div className='flex gap-3'> 
+        <p>Have an account</p>
+        <Link to='/sign-in'>
+          <span className='text-blue-700'>Sign in</span>
+        </Link>
+      </div>
     </div>
   )
 }
